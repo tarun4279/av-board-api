55a5997e36d1a62aa27e69843ecc63163b1e9a4c
Tarun Kundhiya <tkundhiya4279@gmail.com>
2026-02-08 13:06:24 +0530

    Fixing for CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,18 @@ import dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config({ path: '.env.local' });
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api/v1');
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,PATCH,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
+
+
+
 }
 bootstrap();
